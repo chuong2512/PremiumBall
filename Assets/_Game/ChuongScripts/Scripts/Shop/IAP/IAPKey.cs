a93200fd82107f51298432639c8cf13f150814f1
PhanHieu244 <phanhieu2443@gmail.com>
2025-03-14 23:26:07 +0700

    UP

---
+++ Assets/_Game/ChuongScripts/Scripts/Shop/IAP/IAPKey.cs
@@ -0,0 +1,12 @@
+namespace ChuongCustom
+{
+    public static class IAPKey {
+        public const string PACK1 = "1";
+        public const string PACK2 = "2";
+        public const string PACK3 = "3";
+        
+        public const string PACK4 = "4";
+        public const string PACK5 = "5";
+        public const string PACK6 = "6";
+    }
+}
